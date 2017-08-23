b598350dbde25ed9795490f87e3bd4a21e51cd47
karthikrvr <31030146+karthikrvr@users.noreply.github.com>
2017-08-23 11:45:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,8 @@ var htmltemplate = `
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName',function(req, res) {  //articleName == article-one
+app.get('/:articleName',function(req, res) {  
+          //articleName == article-one
          //articles[articleName] == {} content object for article one
     var articleName = req.parases.articleName;
     res.send(createTemplate(articles[articleName]));
