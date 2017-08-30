4a9f6d13ea700920d041f7bec6f3f6f023d68cba
karthikrvr <31030146+karthikrvr@users.noreply.github.com>
2017-08-30 17:16:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ element.innerHTML = 'New value';
 
 //move the image
 var img = document.getElementById('modi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginleft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,100);
 };
