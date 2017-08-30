1975198f67c9e58647f1e399b5554a5931f5645a
karthikrvr <31030146+karthikrvr@users.noreply.github.com>
2017-08-30 11:46:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ var articles = {
 };
 
 function createTemplate (data) {
-    var title = data.title;
+    var title = data.tittle;
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
