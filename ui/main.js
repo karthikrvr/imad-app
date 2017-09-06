03dbbe063d51adbba54b6bf807ff3768bff8dd05
karthikrvr <31030146+karthikrvr@users.noreply.github.com>
2017-09-06 17:01:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,6 +18,6 @@ button.onclick = function () {
         }
     };
     
-    request.open('GET','http://coco98.imad.hasura-app.io/counter',true);
+    request.open('GET','http://karthiksajja.imad.hasura-app.io/counter',true);
     request.send(null);
 };
