26506171c8513210174359e01cf6a82aca68ba77
karthikrvr <31030146+karthikrvr@users.noreply.github.com>
2017-09-16 15:00:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,7 @@ button.onclick = function () {
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
    //make a request to the server and send the name
@@ -50,6 +49,8 @@ submit.onclick = function () {
         }
     };
     
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://karthiksajja.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 };
