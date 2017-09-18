1b776933aedd1f782bc58554ba829ce3e62c3150
karthikrvr <31030146+karthikrvr@users.noreply.github.com>
2017-09-18 15:16:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'karthiksajja',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.enc.DB_PASSWORD
+    password: process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
